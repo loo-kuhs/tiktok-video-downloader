a6c0a96aaba751ae4a1b1cb38a2c3f0ca15c6be4
loo-kuhs <rukazu89@gmail.com>
2023-02-28 22:57:07 -0600

    Add comentaries to ObtainData.js

--- src/ObtainData.js
+++ src/ObtainData.js
@@ -7,6 +7,11 @@ class SanitizeURL {
     this.#url = url
   }
 
+  /**
+   * It takes the URL of the video, splits it into an array, then takes the last element of the array
+   * and assigns it to the videoID property.
+   * @returns {string} The video ID.
+   */
   getVideoID() {
     let url = this.#url.split('?')
     url = url[0].split('/')
@@ -15,6 +20,11 @@ class SanitizeURL {
     return this.#videoID
   }
 
+  /**
+   * It splits the url into an array, then it splits the url again, then it replaces the @ symbol with
+   * nothing, then it sets the username to the url, then it returns the username.
+   * @returns {string} The username of the user who posted the TikTok video.
+   */
   getUsername() {
     let url = this.#url.split('?')
     url = url[0].split('/')
@@ -24,10 +34,18 @@ class SanitizeURL {
     return this.#username
   }
 
+  /**
+   * It returns a string that is the URL of the TikTok video.
+   * @returns {string} The return value is a string.
+   */
   getTikTokVideoURL() {
     return `https://www.tiktok.com/@${this.getUsername()}/video/${this.getVideoID()}`
   }
 
+  /**
+   * It returns the download URL of the video
+   * @returns {string} The download URL of the video.
+   */
   getDownloadURL() {
     return `https://www.tikwm.com/video/media/hdplay/${this.getVideoID()}.mp4`
   }
