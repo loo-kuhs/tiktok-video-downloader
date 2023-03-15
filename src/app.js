a6e3e44cb11e916ed967051266cc9c560e2d553d
loo-kuhs <rukazu89@gmail.com>
2023-03-14 22:14:12 -0600

    Add urls extractor

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ program
   .option(`--url <url>`, 'URL to download')
   .option(`--file <file>`, 'File containing URLs to download')
   .option(`--read-file <file>`, 'File containing URLs to download')
-  .option(`--extract-urls <file>`, 'Extract URLs from a TXT file with HTML content and save them to a TXT file')
+  .option(
+    `--extract-urls <file>`,
+    'Extract URLs from a TXT file with HTML content and save them to a TXT file'
+  )
   .option(`-u, --username <username>`, 'Username to download videos from')
   .parse(process.argv)
 
