b8ae9ed9aac695fa303e145c01fcb42c993cb268
loo-kuhs <rukazu89@gmail.com>
2023-07-20 13:43:57 -0600

    Remove unnecesary comments

--- src/helpers/UrlLoader.js
+++ src/helpers/UrlLoader.js
@@ -12,21 +12,10 @@ class UrlLoader {
   #urls
   #fileName
 
-  /**
-   * Creates an instance of UrlLoader.
-   * @param {string} fileName - The name of the file to load the URLs from.
-   * @memberof UrlLoader
-   */
   constructor(fileName) {
     this.#fileName = fileName
   }
 
-  /**
-   * Loads the URLs from the file and assigns them to the #urls property.
-   * @method
-   * @throws {Error} If the file is empty.
-   * @memberof UrlLoader
-   */
   loadUrls() {
     const fileContent = fs.readFileSync(this.#fileName, 'utf-8')
     const urls = fileContent.replace(/(?:\s*(?:\#.*\n)?)+/, '')
@@ -37,37 +26,16 @@ class UrlLoader {
     }
   }
 
-  /**
-   * Gets the number of URLs loaded from the file.
-   * @method
-   * @throws {Error} If no URLs have been loaded.
-   * @returns {number} The number of URLs loaded.
-   * @memberof UrlLoader
-   */
   getUrlsCount() {
     this.validateUrlsLoaded()
     return this.#urls.length
   }
 
-  /**
-   * Gets the array of URLs loaded from the file.
-   * @method
-   * @throws {Error} If no URLs have been loaded.
-   * @return {string[]} The array of URLs loaded from the file.
-   * @memberof UrlLoader
-   */
   getUrlsArray() {
     this.validateUrlsLoaded()
     return this.#urls
   }
 
-  /**
-   * Adds a URL to the array of URLs.
-   * @method
-   * @throws {Error} If the URL is not a string or is empty.
-   * @param {string} url - The URL to add to the array of URLs.
-   * @memberof UrlLoader
-   */
   addUrl(url) {
     this.validateUrlsLoaded()
     if (typeof url !== 'string' || url.trim() === '') {
@@ -76,13 +44,6 @@ class UrlLoader {
     this.#urls.push(url.trim())
   }
 
-  /**
-   * Removes a URL from the array of URLs.
-   * @method
-   * @throws {Error} If the index is not a number or is not between 0 and the number of URLs.
-   * @param {string} index - The index of the URL to remove from the array of URLs.
-   * @memberof UrlLoader
-   */
   removeUrl(index) {
     this.validateUrlsLoaded()
     if (typeof index !== 'number' || index < 0 || index > this.#urls.length) {
@@ -93,33 +54,15 @@ class UrlLoader {
     this.#urls.splice(index, 1)
   }
 
-  /**
-   * Gets the name of the file that the URLs are loaded from.
-   * @method
-   * @return {string} The name of the file that the URLs are loaded from.
-   * @memberof UrlLoader
-   */
   getFileName() {
     const fileName = this.#fileName.split('/')
     return fileName[fileName.length - 1]
   }
 
-  /**
-   * Sets the name of the file that the URLs are loaded from.
-   * @method
-   * @param {string} fileName - The name of the file that the URLs are loaded from.
-   * @memberof UrlLoader
-   */
   setFileName(fileName) {
     this.#fileName = fileName
   }
 
-  /**
-   * Validates that the #urls property is not empty.
-   * @method
-   * @throws {Error} If the #urls property is empty.
-   * @memberof UrlLoader
-   */
   validateUrlsLoaded() {
     if (!this.#urls) {
       throw new Error('No URLs loaded.')
